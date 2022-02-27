a23745b9ca107407e288a9c1d765abb4317d2133
osman <osmangani360@gmail.com>
2022-02-27 10:41:02 -0500

    run Browser

---
+++ src/test/java/Login.java
@@ -0,0 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Login {
+
+
+    @Test
+    public void login_test() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://qa.taltektc.com/home/login");
+    }
+}
